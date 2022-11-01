4e186fc71e0df10556155d9cefeb477db6faaf9c
jcablocal <jcablocal@gmail.com>
2022-11-01 15:48:07 +0200

    change title on resources/views/base/include-seo.blade.php and update more infor on resources/views/landing/footer.blade.php

--- resources/views/landing/footer.blade.php
+++ resources/views/landing/footer.blade.php
@@ -12,21 +12,21 @@
                     </div>
                     <ul class="footer-links list-reset">
                         <li>
-                            <a href="#">Contact</a>
+                            <a href="/contatos">Contactos</a>
                         </li>
                         <li>
-                            <a href="#">About us</a>
+                            <a href="/sobrenos">Sobre nos</a>
                         </li>
                         <li>
-                            <a href="#">FAQ's</a>
+                            <a href="/faq">FAQ's</a>
                         </li>
                         <li>
-                            <a href="#">Support</a>
+                            <a href="/suporte">Suporte</a>
                         </li>
                     </ul>
                     <ul class="footer-social-links list-reset">
                         <li>
-                            <a href="#">
+                            <a href="https://www.google.com/search?q=facebook+bolos&sxsrf=ALiCzsaxuTG6OdKW-XRy6v4hF-doKSx5GA%3A1667308210728&ei=shphY4aVLMybhbIP7t2S8Ac&ved=0ahUKEwiGtoaNh437AhXMTUEAHe6uBH4Q4dUDCA8&uact=5&oq=facebook+bolos&gs_lp=ugYGCAEQARgJugYGCAIQARgIEgxnd3Mtd2l6LXNlcnC4AQP4AQEyBBAjGCcyBhAAGAcYHjIGEAAYBxgeMgYQABgIGB4yBhAAGAgYHjIGEAAYCBgeMgYQABgIGB4yBhAAGAgYHjIGEAAYCBgeMgYQABgIGB7CAgoQABhHGNYEGLADwgIHEAAYsAMYQ8ICDhAAGOQCGNYEGLAD2AEBwgINEC4YyAMYsAMYQ9gBAsICBxAjGLACGCfCAg0QABiABBixAxiDARgNwgIHEAAYgAQYDcICCBAAGAgYBxgekAYTSNQdUIsKWOgacAF4AcgBAJABAJgBrgOgAboWqgEHMi01LjIuMuIDBCBBGADiAwQgRhgBiAYB&sclient=gws-wiz-serp" target="_blank">
                                 <span class="screen-reader-text">Facebook</span>
                                 <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M6.023 16L6 9H3V6h3V4c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V6H13l-1 3H9.28v7H6.023z" fill="#0270D7"/>
@@ -34,7 +34,7 @@
                             </a>
                         </li>
                         <li>
-                            <a href="#">
+                            <a href="https://www.google.com/search?q=twitter+bolos&sxsrf=ALiCzsZoedpYR9rxGiHKY71SFMfD6D4gLw%3A1667308073530&ei=KRphY4P_H9GO8gLd_6q4Cg&oq=twi+bolos&gs_lp=Egxnd3Mtd2l6LXNlcnC4AQP4AQEqAggAMgYQABgHGB4yChAAGAgYBxgeGA8yCBAAGAUYBxgewgIIEAAYgAQYsAPCAgkQABgIGB4YsAPCAgoQLhixAxiDARhDwgIHEC4YsQMYQ8ICBBAAGEPCAgQQLhhDwgIIEAAYBxgeGArCAgoQABgHGB4YDxgKkAYKSNpEUIISWIcycAF4AMgBAJABAJgBxgOgAfgbqgEHMi01LjUuMeIDBCBBGAHiAwQgRhgAiAYB&sclient=gws-wiz-serp" target="_blank">
                                 <span class="screen-reader-text">Twitter</span>
                                 <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M16 3c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4C.7 7.7 1.8 9 3.3 9.3c-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H0c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4C15 4.3 15.6 3.7 16 3z" fill="#0270D7"/>
@@ -42,7 +42,7 @@
                             </a>
                         </li>
                         <li>
-                            <a href="#">
+                            <a href="https://www.google.com/search?q=bolos&sxsrf=ALiCzsZl2LFg3PqGULksACHUwqbMel3zzA%3A1667308176653&ei=kBphY7S4J9WX8gLc2p2oBw&ved=0ahUKEwi0wOb8ho37AhXVi1wKHVxtB3UQ4dUDCA8&uact=5&oq=bolos&gs_lp=Egxnd3Mtd2l6LXNlcnC4AQP4AQEyChAuGLEDGIMBGEMyBxAuGLEDGEMyBBAAGEMyBBAAGEMyBBAAGEMyBBAuGEMyBhAAGAcYHjIEEAAYQzIGEAAYBxgeMgUQLhiABEjvJlCrFVjxIXABeADIAQCQAQGYAZUEoAHgFKoBCTItMy40LjAuMeIDBCBBGAHiAwQgRhgAiAYB&sclient=gws-wiz-serp" target="_blank">
                                 <span class="screen-reader-text">Google</span>
                                 <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M7.9 7v2.4H12c-.2 1-1.2 3-4 3-2.4 0-4.3-2-4.3-4.4 0-2.4 2-4.4 4.3-4.4 1.4 0 2.3.6 2.8 1.1l1.9-1.8C11.5 1.7 9.9 1 8 1 4.1 1 1 4.1 1 8s3.1 7 7 7c4 0 6.7-2.8 6.7-6.8 0-.5 0-.8-.1-1.2H7.9z" fill="#0270D7"/>
@@ -50,7 +50,7 @@
                             </a>
                         </li>
                     </ul>
-                    <div class="footer-copyright">&copy; 2019 Solid, all rights reserved</div>
+                    <div class="footer-copyright">&copy; 2022 Bolo caseiro, todos os direitos reservados</div>
                 </div>
             </div>
         </footer>
