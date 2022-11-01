d7b7c0ccdc4d23cae92f3e0bbbca18b0328f58ec
jcablocal <jcablocal@gmail.com>
2022-11-01 13:58:54 +0200

    resources/views/landing.blade.php change style of logo

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -14,7 +14,11 @@
                     <div class="brand header-brand">
                         <h1 class="m-0">
 							<a href="#">
-								<img class="header-logo-image" src="{{ asset('contents/logo.jpg')}}" alt="Logo">
+								<img 
+                                    class="header-logo-image" 
+                                    src="{{ asset('contents/logo.jpg')}}" 
+                                    alt="Logo"
+                                    style="width; 50px; height: 50px">
                             </a>
                         </h1>
                     </div>
